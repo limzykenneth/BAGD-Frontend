6c1e571273619bd2a29d741de1ee05646406198b
limzykenneth <limzy.kenneth@gmail.com>
2017-05-21 15:59:03 +0100

    Use local copy of data, CSM server not stable

--- javascripts/collection.js
+++ javascripts/collection.js
@@ -8,9 +8,8 @@ var model = require("./models.js");
 var collection = Backbone.Collection.extend({
 	model: model,
 	// url: "http://info.csmgraphicdesign.com/wp-json/wp/v2/student_info?filter[posts_per_page]=-1",
-	url: "http://infocsmgraphics.x10host.com/wp-json/wp/v2/student_info?filter[posts_per_page]=-1", // backup
 	// url: "http://localhost/wordpress/wp-json/wp/v2/student_info",
-	// url: "./response.json",
+	url: "./response-local.json",
 
 	comparator: "name"
 });
